ca5f645416f9670fb3ecb742d33228784108402f
YessineAmor <yassine.amor@esprit.tn>
2023-02-19 17:59:11 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import NotionBoard from './components/NotionBoard';
+
+function App() {
+  return (
+    <div>
+      <NotionBoard />
+    </div>
+  );
+}
+
+export default App;
